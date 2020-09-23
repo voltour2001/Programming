b5c4c0e556a00ecbf4194539e5b2ab5c44d4180b
voltour2001 <1spirospanagiotis@gmail.com>
2020-09-23 19:11:44 +0300

    i dunno

--- VSCode/Chapters/flask/main.py
+++ VSCode/Chapters/flask/main.py
@@ -1,10 +1,26 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.secret_key= "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHENT_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
 
 @app.route("/")
 def home():
@@ -16,25 +32,51 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"]= user
+        
+        found_user = users.query.filter_by(name=user).first()
+        if :
+
+
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.commit()
+
+        flash("Login Succesful!")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("Already logged in!")
             return redirect(url_for("user"))
 
         return render_template("login.html")
     
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return F"<h1>{user}</h1>"
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Email was saved!")
+        else:
+            if "email" in session:
+                email = session["email"]
+
+        return render_template("user.html", email=email)
     else:
+        flash("You are not logged in!")
         return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
+    flash("You logged out succesfully", "info")
     session.pop("user", None)
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
