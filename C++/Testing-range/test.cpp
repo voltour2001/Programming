657daf0485764be7a79f48c63964bb80bc9e2efb
Spiros-Sys <2spirospanagiotis@gmail.com>
2021-05-19 10:34:06 +0300

    added some stuff

--- C++/Testing-range/test.cpp
+++ C++/Testing-range/test.cpp
@@ -2,15 +2,5 @@
 #include <vector>
 
 int main() {
-    
-    std::vector<int> num = {0, 10, 20};
 
-    for (int i = 0; i < num.size(); i++) {
-    
-    num[i] = num[i] + 10;
-    
-    }
-    for (int i = 0; i < num.size(); i++){
-        std::cout << num[i] << "\n";
-    }
-}
+}   
