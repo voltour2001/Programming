d5bc00fc6225e6258541baeeeafca1ded07dd795
voltour2001 <1spirospanagiotis@gmail.com>
2021-05-19 11:01:03 +0300

    added to notes

--- C++/Testing-range/test.cpp
+++ C++/Testing-range/test.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <vector>
 
+void print_name(int x, std::string name){
+
+    std::cout << "You have an ugly name, " << name << ", "<< x << " times uglier than average.";
+
+}
+
 int main() {
+
+    print_name(5, "Spiros");
 
 }   
