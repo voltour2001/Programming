257c26acc765baebedd19ef2f862d9787e2740c4
voltour2001 <1spirospanagiotis@gmail.com>
2022-12-12 01:05:19 +0200

    a

--- C++/testing-range/main.cpp
+++ C++/testing-range/main.cpp
@@ -9,5 +9,13 @@ int main()
 {
   int x;
   cout << "Hello World!" << '\n';
+  std::cout << "Enter your grade : ";
+  std::cin >> x;
+  if (x<10) {
+    std::cout << "you have failed" << '\n';
+  }
+  else{
+    std::cout << "you passed with : " << x << '\n';
+  }
   return 0;
 }
