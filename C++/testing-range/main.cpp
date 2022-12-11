47ee292b43174dbcfbab099074ce5b41d362c458
voltour2001 <1spirospanagiotis@gmail.com>
2022-12-12 01:48:21 +0200

    Test Complete

--- C++/testing-range/main.cpp
+++ C++/testing-range/main.cpp
@@ -13,6 +13,8 @@ int main()
   cout << "Hello World!" << '\n';
   std::cout << "Enter your grade : ";
   std::cin >> x;
+  
+  // Test If Loop
   if (x<10) {
     std::cout << "you have failed" << '\n';
   }
@@ -20,14 +22,12 @@ int main()
     std::cout << "you passed with : " << x << '\n';
   }
   int i = 100;
-  /* this is a comment */
+  /* Test Loop */ 
   for (int i = 0; i < 3; i++)
   {
     cout << name << endl;
   }
+  // Another Comment
   
-
-  
-  /* Another Comment */
   return 0;
 }
