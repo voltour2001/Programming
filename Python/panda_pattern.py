9982b464955e3959a4fadfe323a0ad5baebad622
voltour2001 <56365033+voltour2001@users.noreply.github.com>
2022-12-12 20:23:08 +0200

    Update panda_pattern.py

--- Python/panda_pattern.py
+++ Python/panda_pattern.py
@@ -1,15 +1,6 @@
 # Draw a Panda using Turtle Graphics
 # Import turtle package
 
-# //awd ads
-# //adasasd
-# //wadaw da w
-# //awdawd aw
-# //awdawdawd wd
-# // ! wadopamwdopwadm
-
-
-
 import turtle
 
 # Creating a turtle object(pen)
