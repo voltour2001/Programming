41704b499cd00a90ebd36c0c8871981e6b95041e
voltour2001 <1spirospanagiotis@gmail.com>
2022-12-13 18:31:47 +0200

    test

--- C++/x/test.cpp
+++ C++/x/test.cpp
@@ -1,25 +1,13 @@
 // Factorial of n = 1*2*3*...*n
 
 #include <iostream>
-using namespace std;
 
-int factorial(int);
+int main(int argc, char const *argv[])
+{
+    int x;
+    std::cout << "hello" << std::endl;
+    
+    std::cin >> x;
 
-int main() {
-    int n, result;
-
-    cout << "Enter a non-negative number: ";
-    cin >> n;
-
-    result = factorial(n);
-    cout << "Factorial of " << n << " = " << result;
     return 0;
-}
-
-int factorial(int n) {
-    if (n > 1) {
-        return n * factorial(n - 1);
-    } else {
-        return 1;
-    }
 }
