91da8e08ff1e70349de6465a8fd86f6431480d76
voltour2001 <1spirospanagiotis@gmail.com>
2023-02-19 18:27:40 +0200

    test push

--- C++/fstream/main.cpp
+++ C++/fstream/main.cpp
@@ -4,17 +4,26 @@
 using namespace std;
 
 int main(){
-    
-    fstream myFile;
-    /* 
-    Write mode  = ios::out
-    Append Mode = ios::app
-    Read Mode   = ios::in 
-    */
-    myFile.open("data.txt", ios::app); 
-    if(myFile.is_open()){
-        myFile << "Username\nPassword" << endl;
-        myFile.close();
+
+fstream myFile;
+/* 
+Write mode  = ios::out
+Append Mode = ios::app
+Read Mode   = ios::in 
+*/
+myFile.open("data.txt", ios::out); 
+if(myFile.is_open()){
+    myFile << "Username\nPassword" << endl;
+    myFile.close();
+}
+
+myFile.open("data.txt", ios::in); 
+if(myFile.is_open()){
+    string line;
+    while (getline(myFile, line)){
+            std::cout << line << std::endl;
     }
+    myFile.close();
+}
 
 }
