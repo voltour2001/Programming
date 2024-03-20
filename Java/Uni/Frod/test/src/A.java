09d0c3ec7b2f9ff0449022dea76d3164a5b5bf05
voltour2001 <1spirospanagiotis@gmail.com>
2024-03-20 18:20:52 +0200

    new

---
+++ Java/Uni/Frod/test/src/A.java
@@ -0,0 +1,12 @@
+class A{
+    public static int findMax(int a, int b, int c){
+        int max = a;
+        if (b>max) {
+            max = b;
+        }
+        if (c>max) {
+            max = c;
+        }
+        return max;
+    }
+}
---
+++ Java/Uni/Frod/test/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        A test = new A();
+        int max = test.findMax(5, 10, 20);      
+        System.out.println(max);
+    }
+}
