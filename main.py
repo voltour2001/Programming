9f7c8d77edeb08f5833c0d92c91a8c2db5d369c6
voltour2001 <1spirospanagiotis@gmail.com>
2024-04-05 21:25:31 +0300

    removed test python files

--- main.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Tuple
-
-def square_numbers(nums: List[float]) -> List[Tuple[float, float]]:
-    # Calculate the square of each number in the given list
-    return [(num, num**2) for num in nums]
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result = square_numbers(numbers)
-print(result)
--- test.py
+++
@@ -1,6 +0,0 @@
-def print_squared_numbers(numbers):
-    for num in numbers:
-        print(num ** 2)
-
-numbers = [1, 2, 3, 4, 5]
-print_squared_numbers(numbers)
