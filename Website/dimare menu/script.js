0bbdb37688642f7101bd8c9024d81744fca98e19
voltour2001 <1spirospanagiotis@gmail.com>
2024-07-08 21:07:16 +0300

    latest version

--- Website/dimare menu/script.js
+++ Website/dimare menu/script.js
@@ -1,5 +1,8 @@
 // Function to toggle hamburger menu
 function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    const exitButton = document.querySelector('.exit-button');
+
     navMenu.classList.toggle('show');
     exitButton.style.display = navMenu.classList.contains('show') ? 'block' : 'none';
 }
@@ -21,11 +24,31 @@ function closeModal() {
 
 document.addEventListener('DOMContentLoaded', function() {
     const menuSections = document.querySelectorAll('.menu-section h3');
+    const navMenu = document.querySelector('.nav-menu');
 
+    // Toggle visibility of menu sections
     menuSections.forEach(section => {
         section.addEventListener('click', () => {
             const ul = section.nextElementSibling;
             ul.classList.toggle('hidden');
+        });
+    });
+
+    // Event listener for all links inside the nav menu
+    navMenu.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default link behavior
+            const targetId = this.getAttribute('href').substring(1); // Get target section id
+            const targetSection = document.getElementById(targetId); // Find the target section
+
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth'
+                });
+
+                // Close menu after clicking a link
+                navMenu.classList.remove('show');
+            }
         });
     });
 });
