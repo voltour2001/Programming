ebcb287d997e4ecec4df8a295bfaeb41ee5cba17
voltour2001 <1spirospanagiotis@gmail.com>
2024-10-15 17:28:28 +0300

    removed test files and folders

--- Java/Uni/Frod/test/src/tempCodeRunnerFile.java
+++
@@ -1,2 +0,0 @@
-    System.out.println("s1 address="+ Integer.toHexString(System.identityHashCode(str1)));
-    System.out.println("s1 address="+ Integer.toHexString(System.identityHashCode(str2)));
